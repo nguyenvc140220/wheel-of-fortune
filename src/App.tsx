de5abfceb2419c0baf1ae8f15fb0ef5fdb8f1ff6
nguyenvc <nguyenvc@metech.com>
2024-05-20 18:47:44 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import { Wheel } from 'react-custom-roulette';
+
+
+const data = [
+  { option: 'REACT' },
+  { option: 'CUSTOM' },
+  { option: 'ROULETTE', style: { textColor: '#f9dd50' } },
+  { option: 'WHEEL' },
+  { option: 'REACT' },
+  { option: 'CUSTOM' },
+  { option: 'ROULETTE', style: { textColor: '#70bbe0' } },
+  { option: 'WHEEL' },
+];
+
+const backgroundColors = ['#ff8f43', '#70bbe0', '#0b3351', '#f9dd50'];
+const textColors = ['#0b3351'];
+const outerBorderColor = '#eeeeee';
+const outerBorderWidth = 10;
+const innerBorderColor = '#30261a';
+const innerBorderWidth = 0;
+const innerRadius = 0;
+const radiusLineColor = '#eeeeee';
+const radiusLineWidth = 8;
+const fontFamily = 'Nunito';
+const fontWeight = 'bold';
+const fontSize = 20;
+const fontStyle = 'normal';
+const textDistance = 60;
+const spinDuration = 1.0;
+
+const App = () => {
+  const [mustSpin, setMustSpin] = useState(false);
+  const [prizeNumber, setPrizeNumber] = useState(0);
+
+  const handleSpinClick = () => {
+    if (!mustSpin) {
+      const newPrizeNumber = Math.floor(Math.random() * data.length);
+      setPrizeNumber(newPrizeNumber);
+      setMustSpin(true);
+    }
+  };
+
+  return (
+    <div className="App">
+        <header className="App-header">
+          <div className={'parent-container'}>
+            <Wheel
+                mustStartSpinning={mustSpin}
+                prizeNumber={prizeNumber}
+                data={data}
+                backgroundColors={backgroundColors}
+                textColors={textColors}
+                fontFamily={fontFamily}
+                fontSize={fontSize}
+                fontWeight={fontWeight}
+                fontStyle={fontStyle}
+                outerBorderColor={outerBorderColor}
+                outerBorderWidth={outerBorderWidth}
+                innerRadius={innerRadius}
+                innerBorderColor={innerBorderColor}
+                innerBorderWidth={innerBorderWidth}
+                radiusLineColor={radiusLineColor}
+                radiusLineWidth={radiusLineWidth}
+                spinDuration={spinDuration}
+                startingOptionIndex={2}
+                // perpendicularText
+                textDistance={textDistance}
+                onStopSpinning={() => {
+                  setMustSpin(false);
+                }}
+            />
+            <button className={'spin-button'} onClick={handleSpinClick}>
+              SPIN
+            </button>
+          </div>
+        </header>
+      </div>
+  );
+};
+
+export default App;
